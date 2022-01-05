2f4f13e2bf377f9c99abf0fc08838edc6a4fa480
mingfang <mingfang@mac.com>
2022-01-05 18:31:26 -0500

    use logical home directory

--- user/main.tf
+++ user/main.tf
@@ -1,13 +1,18 @@
-resource "tls_private_key" "key" {
-  algorithm = "RSA"
-  rsa_bits  = "4096"
-}
-
 resource "aws_transfer_user" "transfer_server_user" {
   server_id      = var.server_id
   user_name      = var.user_name
   role           = var.role
-  home_directory = format("/%s/%s", var.s3_bucket_id, var.folder)
+
+  home_directory_type = "LOGICAL"
+  home_directory_mappings {
+    entry = "/"
+    target = "/${var.s3_bucket_id}/${var.user_name}"
+  }
+}
+
+resource "tls_private_key" "key" {
+  algorithm = "RSA"
+  rsa_bits  = "4096"
 }
 
 resource "aws_transfer_ssh_key" "transfer_server_ssh_key" {
